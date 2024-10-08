492efa5cbe7ce06e60c204dc71f53f2090d7c7b4
ad-303 <anand.ad@northeastern.edu>
2024-10-08 13:34:08 -0400

    Added Lab 3 with Screenshots

---
+++ lab3/Lab3_Navigation/src/model/Patient.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author Adithya Anand
+ */
+public class Patient {
+    private String name;
+    private String gender;
+    private String patientType;
+    private ImageIcon profilePicture;
+    public Icon ProfilePicuture;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPatientType() {
+        return patientType;
+    }
+
+    public void setPatientType(String patientType) {
+        this.patientType = patientType;
+    }
+
+    public ImageIcon getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(ImageIcon profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+    
+    
+}
+
